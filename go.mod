ae43a2b092af538b91ba5f9e5050fd177ae30989
Harry Pidcock <haza55@gmail.com>
2019-04-19 22:52:04 +1000

    Added basic resolver

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/hpidcock/nz
+
+go 1.12
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/m13253/dns-over-https v1.4.2
+	github.com/miekg/dns v1.1.8
+	github.com/tonnerre/golang-dns v0.0.0-20130925195549-c07f3c3cc475 // indirect
+	golang.org/x/net v0.0.0-20190419010253-1f3472d942ba // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"time"
+
+	jsonDNS "github.com/m13253/dns-over-https/json-dns"
+
+	dns "github.com/miekg/dns"
+)
+
+var upstream = []string{
+	"https://1.1.1.1/dns-query",
+	"https://dns.google.com/resolve",
+}
+
+var errBadStatus = errors.New("bad status")
+
+var client *http.Client
+
+func main() {
+	cookies, _ := cookiejar.New(nil)
+	client = &http.Client{
+		Timeout: 5 * time.Second,
+		Jar:     cookies,
+	}
+
+	dns.HandleFunc(".", handleDNSRequest)
+	nss := dns.Server{Addr: "127.0.0.1:53", Net: "udp"}
+	err := nss.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func doh(remote string, r *dns.Msg) (*dns.Msg, error) {
+	q := r.Question[0]
+
+	u := fmt.Sprintf("%s?name=%s&type=%d", remote, q.Name, q.Qtype)
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("accept", "application/dns-json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errBadStatus
+	}
+
+	dnsJSONRes := jsonDNS.Response{}
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&dnsJSONRes)
+	if err != nil {
+		return nil, err
+	}
+
+	reply := jsonDNS.PrepareReply(r)
+	reply = jsonDNS.Unmarshal(reply, &dnsJSONRes, dns.DefaultMsgSize, 255)
+	return reply, nil
+}
+
+func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	log.Printf("got question from %s\n", w.RemoteAddr().String())
+
+	if r.Response {
+		log.Print("got response message as request")
+		w.Close()
+		return
+	}
+
+	if len(r.Question) != 1 {
+		log.Printf("got message for %d questions\n", len(r.Question))
+		w.Close()
+		return
+	}
+
+	type response struct {
+		err error
+		msg *dns.Msg
+	}
+	rc := make(chan response, len(upstream))
+
+	for _, v := range upstream {
+		remote := v
+		go func() {
+			res, err := doh(remote, r)
+			rc <- response{err, res}
+		}()
+	}
+
+	for range upstream {
+		res := <-rc
+		if res.err != nil {
+			log.Print(res.err)
+			continue
+		}
+
+		w.WriteMsg(res.msg)
+		return
+	}
+
+	w.Close()
+}
