0c1f5433d7e2a941d7891f255c3d4edfeab29766
Harry Pidcock <haza55@gmail.com>
2019-04-21 14:48:23 +1000

    Prevent plaintext connections, tighten TLS configuration, use HTTP2

--- main.go
+++ main.go
@@ -1,38 +1,100 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"net/http/cookiejar"
+	"strings"
 	"time"
+
+	"golang.org/x/net/http2"
 
 	jsonDNS "github.com/m13253/dns-over-https/json-dns"
 
 	dns "github.com/miekg/dns"
 )
 
-var upstream = []string{
-	"https://1.1.1.1/dns-query",
-	"https://dns.google.com/resolve",
-}
+var (
+	errBadStatus = errors.New("bad status")
+)
 
-var errBadStatus = errors.New("bad status")
+var (
+	flagListen       = flag.String("listen", "127.0.0.2:53", "internal dns listen address")
+	flagAllowCookies = flag.Bool("allow-cookies", true, "allow cookies to be stored in the DNS-over-HTTPs client")
+	flagTimeout      = flag.Duration("timeout", 5*time.Second, "timeout on upstream https requests")
+	flagUpstream     = flag.String("upstream", "https://1.1.1.1/dns-query,https://dns.google.com/resolve",
+		"comma delimited list of upstream DNS-over-HTTPs resolvers")
+)
 
+var upstream = []string{}
 var client *http.Client
 
 func main() {
-	cookies, _ := cookiejar.New(nil)
+	var err error
+	flag.Parse()
+
+	upstream = strings.Split(*flagUpstream, ",")
+
+	var cookies http.CookieJar
+	if *flagAllowCookies {
+		cookies, _ = cookiejar.New(nil)
+	}
+
+	tlsConfig := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		MaxVersion:               tls.VersionTLS13,
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+		},
+	}
+
+	transport := &http.Transport{
+		DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+			return nil, io.ErrClosedPipe
+		},
+		Dial: func(_, _ string) (net.Conn, error) {
+			return nil, io.ErrClosedPipe
+		},
+		DialTLS: func(network, addr string) (net.Conn, error) {
+			return tls.Dial(network, addr, tlsConfig)
+		},
+	}
+
+	err = http2.ConfigureTransport(transport)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	client = &http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: *flagTimeout,
 		Jar:     cookies,
+		CheckRedirect: func(_ *http.Request, _ []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Transport: transport,
 	}
 
 	dns.HandleFunc(".", handleDNSRequest)
-	nss := dns.Server{Addr: "127.0.0.2:53", Net: "udp"}
-	err := nss.ListenAndServe()
+	nss := dns.Server{Addr: *flagListen, Net: "udp"}
+	err = nss.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +110,6 @@ func doh(remote string, r *dns.Msg) (*dns.Msg, error) {
 	}
 
 	req.Header.Set("accept", "application/dns-json")
-
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
